b16bb6f042267bf9598f202f260c7f590df154ca
okiogutu <okiogutu@gmail.com>
2024-10-23 11:54:15 -0700

    review 24

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,27 +2,15 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  /**
-   * should return if Redis is alive
-   * 2 utils created previously:
-   * { "redis": true, "db": true } with a status code 200
-   */
   static getStatus(request, response) {
-    const status = {
-      redis: redisClient.isAlive(),
-      db: dbClient.isAlive(),
-    };
-    response.status(200).send(status);
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  /* should return the number of users and files in DB */
   static async getStats(request, response) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    response.status(200).send(stats);
+    const usersNum = await dbClient.nbUsers();
+    const filesNum = await dbClient.nbFiles();
+    response.status(200).json({ users: usersNum, files: filesNum });
   }
 }
 
-export default AppController;
+module.exports = AppController;
